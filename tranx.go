8fa32904067753d2a38c768d4f70fd13a5b28605
Derek McQuay <dmcquay@apprenda.com>
2017-02-05 00:04:24 -0800

    updated tranx type... again

--- tranx.go
+++ tranx.go
@@ -3,11 +3,11 @@ package chipmunk
 import "time"
 
 type tranx struct {
-	ID          int       `json:"id"`
-	Cost        float64   `json:"cost"`
-	Store       string    `json:"store"`
-	Info        string    `json:"info"`
-	Date        time.Time `json:"date"`
-	User_ID     int       `json:"user_id"`
-	Category_ID int       `json:"category_id"`
+	ID       int       `json:"id"`
+	Cost     float64   `json:"cost"`
+	Store    string    `json:"store"`
+	Info     string    `json:"info"`
+	Date     time.Time `json:"date"`
+	User     int       `json:"user"`
+	Category int       `json:"category"`
 }
